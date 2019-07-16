d5e0f12a26e66a995e5aac38de553f8996dff32e
lyus76 <50140697+lyus76@users.noreply.github.com>
2019-07-16 16:33:53 +0300

    Добавлен выбор группы по индексу

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -13,7 +13,7 @@ public class GroupModificationTests extends TestBase{
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().creareGroup(new GroupData("test1", null, null));
         }
-        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().selectGroup(before - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test1", "test1", "test1"));
         app.getGroupHelper().submitGroupModification();
